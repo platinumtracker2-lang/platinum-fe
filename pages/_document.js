bc5f8c5e0ba5a4ac2e28f39b6e77813063f21ea0
shiv-rishinarayan <shivam.rishinarayansingh@gmail.com>
2026-08-04 00:40:35 +0530

    Add Google Analytics tag (G-2EZ9N9CXHZ)

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,21 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2EZ9N9CXHZ"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2EZ9N9CXHZ');
+            `,
+          }}
+        />
         {/* Favicon */}
         <link rel="icon" href="/Platinum thumbnail.png" type="image/png" />
         <link rel="shortcut icon" href="/Platinum thumbnail.png" type="image/png" />
